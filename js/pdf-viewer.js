f03c4587f8d45009b46e14b8d50ef5b9917839c3
Bluemi <bruno.schilling@protonmail.ch>
2023-10-24 16:29:34 +0200

    fixed click regions

--- js/pdf-viewer.js
+++ js/pdf-viewer.js
@@ -4,7 +4,6 @@ const url = 'https://raw.githubusercontent.com/AzureRaids/AzureRaids.github.io/m
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 const pdfjsLib = window['pdfjs-dist/build/pdf'];
-console.log('pdfjsLib:', pdfjsLib);
 const PDF_BORDER = 10;
 const SCALE_FACTOR = 0.4;
 
@@ -50,45 +49,49 @@ loadingTask.promise.then(function(pdf) {
     // Fetch the first page
     renderPage(pdf, 1);
     renderPage(pdf, 2);
+    setupClickEvents();
 }, function (reason) {
     // PDF loading error
     console.error(reason);
 });
 
-$("#canvas1").on("click", function(evt) {
-    let xPos = (evt.offsetX - PDF_BORDER) / globalScale;
-    let yPos = (evt.offsetY - PDF_BORDER) / globalScale;
-    const circle_fields = [
-        // [[487, 105], 72, 'Azure Raids'],
-        [[48, 471], 24, 'https://www.instagram.com/azure.raids/'],
-        [[114, 471], 24, 'https://www.youtube.com/@LewistonTramps'],
-        [[179, 471], 24, 'https://open.spotify.com/artist/6SI6I1VzjLc5JKi5NhawtA'],
-        [[244, 471], 24, 'https://www.facebook.com/azure.raids'],
-    ];
-    for (let [center, radius, url] of circle_fields) {
-        const distance_squared = Math.pow(xPos - center[0], 2) + Math.pow(yPos - center[1], 2)
-        if (distance_squared < radius * radius) {
-            // console.log(url);
-            window.open(url, '_blank').focus();
-        }
-    }
-    // console.log(xPos, yPos);
-});
-$("#canvas2").on("click", function(evt) {
-    let xPos = (evt.offsetX - PDF_BORDER) / globalScale;
-    let yPos = (evt.offsetY - PDF_BORDER) / globalScale;
-    const rect_fields = [
-        [309, 59, 427, 176, 'https://open.spotify.com/track/1dcJmzAh1ybNK3aNooEN4j'],
-        [457, 59, 572, 176, 'https://open.spotify.com/track/2zW3oVxX9AEszSTWq6qDeL'],
-        [290, 662, 588, 836, 'https://www.youtube.com/watch?v=FE6Bf63v8WI'],
-        [105, 523, 281, 836, 'https://youtu.be/3c-zJfkP6q0'],
-    ]
-    for (let [left, top, right, bot, url] of rect_fields) {
-        if (xPos > left && xPos < right && yPos > top && yPos < bot) {
-            // console.log(url);
-            window.open(url, '_blank').focus();
-        }
-    }
-    // console.log(xPos, yPos);
-});
 
+function setupClickEvents() {
+    $("#canvas1").on("click", function(evt) {
+        let xPos = (evt.offsetX - PDF_BORDER) / globalScale;
+        let yPos = (evt.offsetY - PDF_BORDER) / globalScale;
+        console.log('Pos: ', xPos, ', ', yPos);
+        const circle_fields = [
+            // [[487, 105], 72, 'Azure Raids'],
+            [[52, 502], 24, 'https://www.instagram.com/azure.raids/'],
+            [[126, 502], 24, 'https://www.youtube.com/@LewistonTramps'],
+            [[195, 502], 24, 'https://open.spotify.com/artist/6SI6I1VzjLc5JKi5NhawtA'],
+            [[264, 502], 24, 'https://www.facebook.com/azure.raids'],
+        ];
+        for (let [center, radius, url] of circle_fields) {
+            const distance_squared = Math.pow(xPos - center[0], 2) + Math.pow(yPos - center[1], 2)
+            if (distance_squared < radius * radius) {
+                // console.log(url);
+                window.open(url, '_blank').focus();
+            }
+        }
+        // console.log(xPos, yPos);
+    });
+    $("#canvas2").on("click", function(evt) {
+        let xPos = (evt.offsetX - PDF_BORDER) / globalScale;
+        let yPos = (evt.offsetY - PDF_BORDER) / globalScale;
+        const rect_fields = [
+            [335, 61, 460, 186, 'https://open.spotify.com/track/1dcJmzAh1ybNK3aNooEN4j'],
+            [495, 62, 617, 186, 'https://open.spotify.com/track/2zW3oVxX9AEszSTWq6qDeL'],
+            [312, 708, 619, 899, 'https://www.youtube.com/watch?v=FE6Bf63v8WI'],
+            [115, 560, 305, 897, 'https://youtu.be/3c-zJfkP6q0'],
+        ]
+        for (let [left, top, right, bot, url] of rect_fields) {
+            if (xPos > left && xPos < right && yPos > top && yPos < bot) {
+                // console.log(url);
+                window.open(url, '_blank').focus();
+            }
+        }
+        // console.log(xPos, yPos);
+    });
+}
